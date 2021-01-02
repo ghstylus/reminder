16caee48250845af8bb8dc2805d5224b4ed6daf2
yoohong <ghstylus@gmail.com>
2021-01-02 16:44:09 +0900

    first commit

---
+++ src/test/java/com/reminder/demo/RemiderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reminder.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemiderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
